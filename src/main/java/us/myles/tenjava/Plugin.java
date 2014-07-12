dc3e7ac4dec5c64f5c13b32973441a06855f1ae8
Myles <mylescomputer@gmail.com>
2014-07-12 16:10:26 +0100

    Minecarts placed on le moon are now lunar rovers! Start driving today!

--- src/main/java/us/myles/tenjava/Plugin.java
+++ src/main/java/us/myles/tenjava/Plugin.java
@@ -7,6 +7,7 @@ import org.bukkit.World.Environment;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import us.myles.tenjava.generators.MoonGenerator;
+import us.myles.tenjava.listeners.LunarRover;
 import us.myles.tenjava.listeners.MoonEffects;
 import us.myles.tenjava.listeners.RocketControllerListener;
 import us.myles.tenjava.tasks.MoonTimedEffects;
@@ -15,6 +16,7 @@ public class Plugin extends JavaPlugin {
 	public void onEnable() {
 		Bukkit.getPluginManager().registerEvents(new RocketControllerListener(this), this);
 		Bukkit.getPluginManager().registerEvents(new MoonEffects(this), this);
+		Bukkit.getPluginManager().registerEvents(new LunarRover(this), this);
 		WorldCreator.name("moon").type(WorldType.NORMAL).environment(Environment.NORMAL).generator(new MoonGenerator()).createWorld();
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new MoonTimedEffects(), 5L, 5L);
 	}
---
+++ src/main/java/us/myles/tenjava/listeners/LunarRover.java
@@ -0,0 +1,61 @@
+package us.myles.tenjava.listeners;
+
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Vehicle;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.vehicle.VehicleUpdateEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.util.Vector;
+
+import us.myles.tenjava.Plugin;
+
+public class LunarRover implements Listener {
+	private Plugin plugin;
+
+	public LunarRover(Plugin plugin) {
+		this.plugin = plugin;
+	}
+
+	@EventHandler
+	public void onInteract(PlayerInteractEvent e) {
+		if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if (e.getItem() == null)
+				return;
+			if (e.getItem().getType() == Material.MINECART) {
+				e.getPlayer().getInventory().remove(e.getItem());
+				Block target = e.getClickedBlock();
+				Minecart minecart = (Minecart) target.getLocation().getWorld().spawnEntity(target.getLocation().add(0, 2, 0), EntityType.MINECART);
+				minecart.setMetadata("rover", new FixedMetadataValue(plugin, true));
+				e.setCancelled(true);
+			}
+		}
+	}
+
+	@EventHandler
+	public void roverUpdate(VehicleUpdateEvent e) {
+		Vehicle vehicle = e.getVehicle();
+		if (vehicle.getMetadata("rover").size() != 0) {
+			if (vehicle.getPassenger() != null) {
+				Player passenger = (Player) vehicle.getPassenger();
+				// distance to ground
+				Location ground = vehicle.getWorld().getHighestBlockAt(vehicle.getLocation()).getLocation();
+				Vector v = passenger.getLocation().getDirection();
+				if (ground.distance(vehicle.getLocation()) > 2 && v.getY() > 0)
+					v.setY(0);
+				vehicle.getWorld().playEffect(vehicle.getLocation(), Effect.SMOKE, 0);
+				vehicle.setVelocity(v);
+
+			}
+		}
+	}
+
+}
